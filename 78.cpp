3ed482e0f6fc49d7e2a4d4af536450de2a3b720c
Kacyn1 <62652276+Kacyn1@users.noreply.github.com>
2022-03-08 17:15:30 +0100

    Add files via upload

---
+++ 78.cpp
@@ -0,0 +1,106 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string SzO(string msg) {
+int S[8] = { int('A'), int('L'), int('G'), int('O'), int('R'), int('Y'), int('T'), int('M') }; int length = msg.length();
+int rest = length % 8;
+    
+    									for (int i = 0; i < 8-rest; i++) {
+        msg += ".";
+    }
+    		
+			
+			length = msg.length();
+for (int i = 0; i < length; i++) {
+S[i % 8] = (S[i % 8] + int(msg[i])) % 128;
+}
+for (int j = 0; j < 8; j++) {
+cout << S[j] << " ";
+}
+cout << endl;
+cout << msg.length()<<endl;
+string wynik = "";
+for (int i = 0; i < 8; i++) {
+wynik += char(65 + S[i] % 26);
+}
+return wynik;
+}
+
+string skrot(string msg) {
+			int S[8] = { int('A'), int('L'), int('G'), int('O'), int('R'), int('Y'), int('T'), int('M') };
+			int length = msg.length();
+			int rest = length % 8;
+for (int i = 0; i < 8 - rest; i++) {
+msg += ".";
+  }
+length = msg.length();
+for (int i = 0; i < length; i++) {
+S[i % 8] = (S[i % 8] + int(msg[i])) % 128;
+  }
+string wynik = "";for (int i = 0; i < 8; i++) {
+wynik += char(65 + S[i] % 26);
+}
+return wynik;}
+string deszyfr(int d, int n, int szyfr[8]) {
+string wynik ="";
+for (int i = 0; i < 8; i++) {
+wynik += char(szyfr[i] * d % n);
+}return wynik;}
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+ifstream fIn1;
+ifstream fIn2;
+ofstream fOut;
+fIn1.open("C:\\podpisy.txt");fIn2.open("C:\\wiadomosci.txt");
+fOut.open("C:\\epodpis_wynik.txt");
+string napis1;
+getline(fIn2, napis1);
+fIn2.close();
+    		fIn2.open("C:\\wiadomosci.txt");
+   		cout << SzO(napis1)<<endl;
+    cout << endl;
+    			for (int i = 0; i < 11; i++) {
+    int tab[8];
+string napis;
+getline(fIn2, napis);
+    for (int j = 0; j < 8; j++) {
+int temp;
+    fIn1 >> temp;
+tab[j] = temp;
+    }
+        			cout << deszyfr(3, 200, tab)<<endl;  
+    }
+    fIn1.close();
+    		fIn2.close();
+    			fIn1.open("C:\\podpisy.txt");
+    				fIn2.open("C:\\wiadomosci.txt");
+    					cout << endl;
+    for (int i = 0; i < 11; i++) {
+    				    int tab[8];
+        					string napis;
+        					getline(fIn2, napis);
+        	for (int j = 0; j < 8; j++) {
+            	int temp;
+            		fIn1 >> temp;
+           		 tab[j] = temp;
+        					}
+
+        if (deszyfr(3, 200, tab) == skrot(napis)) {
+            cout << i + 1 << endl;
+        }}}
+
